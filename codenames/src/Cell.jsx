65bee1c51fbe8a40259b332378530910736be0c6
Alex Lee <cal368@cornell.edu>
2020-04-22 11:56:38 -0400

    Some CSS Changes

--- codenames/src/Cell.jsx
+++ codenames/src/Cell.jsx
@@ -1,12 +1,19 @@
 import React, { useState } from 'react'
+import './App.css'
 
 const ItemCell = ({ itemName, secretColor, spyMaster }) => {
 
   const [display, setDisplay] = useState(false)
 
+  const divStyle = {
+    backgroundColor: (display || spyMaster) ? secretColor : 'gray',
+    width: '200px',
+    
+  }
+
   return (
     <div
-      style={{ backgroundColor: (display || spyMaster) ? secretColor : 'gray' }}
+      style={divStyle}
       onClick={() => setDisplay(true)}>
       <p>{itemName}</p>
     </div>
