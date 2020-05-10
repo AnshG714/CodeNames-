36aaa826f7c205bc973a74c117e22d616eda0da0
Ansh Godha <ag759@cornell.edu>
2020-05-10 23:43:20 +0530

    merge

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const functions = require('./firebase-functions')
 const cors = require('cors');
+const path = require('path')
 
 const app = express();
 const port = 8080;
